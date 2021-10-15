c88a84939ead2b87e77c8d18841be12315af40fb
Roland Urbano <urbano.roland@gmail.com>
2021-10-15 15:29:34 +0200

    Added datasource for DNS records (#69)
    * Added datasource for DNS records
    
    * Added acceptance tests for dns records data source
    
    * Fixed dependency check
    
    * Upgraded helper func and removed depreacted schema fields
    
    * Removed unnecessary type check
    
    Co-authored-by: Roland Urbano <rurbano@anexia-it.com>

---
+++ anxcloud/struct_dns_records.go
@@ -0,0 +1,28 @@
+package anxcloud
+
+import "github.com/anexia-it/go-anxcloud/pkg/clouddns/zone"
+
+func flattenDnsRecords(records []zone.Record) []interface{} {
+	zoneRecords := make([]interface{}, 0, len(records))
+	if len(records) < 1 {
+		return zoneRecords
+	}
+
+	for _, record := range records {
+		m := map[string]interface{}{
+			"identifier": record.Identifier.String(),
+			"immutable":  record.Immutable,
+			"name":       record.Name,
+			"rdata":      record.RData,
+			"region":     record.Region,
+			"type":       record.Type,
+		}
+
+		if record.TTL != nil {
+			m["ttl"] = record.TTL
+		}
+
+		zoneRecords = append(zoneRecords, m)
+	}
+	return zoneRecords
+}
