b1ca00fd339665ea636e6f4e50df848d0c8601f8
Kevin Stiehl <kevin.stiehl@numericas.de>
2021-12-14 08:29:59 +0100

    add recorders

---
+++ anxcloud/testutils/recorder/vm_recorder.go
@@ -0,0 +1,80 @@
+package recorder
+
+import (
+	"context"
+	"errors"
+	"github.com/anexia-it/go-anxcloud/pkg/api"
+	"github.com/anexia-it/go-anxcloud/pkg/client"
+	"github.com/anexia-it/go-anxcloud/pkg/vsphere/provisioning/vm"
+	"github.com/anexia-it/go-anxcloud/pkg/vsphere/search"
+	"net/http"
+)
+
+type vmCleanUpFunc func(ctx context.Context) error
+type VMRecoder struct {
+	handlers []vmCleanUpFunc
+	client   api.API
+}
+
+func (v VMRecoder) Cleanup(ctx context.Context) []error {
+	var cleanUpErrors []error
+	for _, handler := range v.handlers {
+		err := handler()
+		if err != nil {
+			cleanUpErrors = append(cleanUpErrors, err)
+		}
+	}
+	return nil
+}
+
+func (v *VMRecoder) RecordVMByName(name string) {
+	v.handlers = append(v.handlers, v.createCleanupHanlderByName(name))
+}
+
+func (v *VMRecoder) RecordVMByID(id string) {
+	v.handlers = append(v.handlers, v.createCleanupHanlderByName(id))
+}
+
+func (v VMRecoder) createCleanupHandlerByID(id string) vmCleanUpFunc {
+	return func(ctx context.Context) error {
+		anxClient, err := client.New(client.TokenFromEnv(false))
+		if err != nil {
+			return err
+		}
+
+		vmAPI := vm.NewAPI(anxClient)
+		_, err = vmAPI.Deprovision(ctx, id, false)
+
+		// it's not an error when it's gone
+		var responseErr client.ResponseError
+		if errors.As(err, &responseErr) && responseErr.Response.StatusCode != http.StatusNotFound {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func (v VMRecoder) createCleanupHanlderByName(name string) vmCleanUpFunc {
+	return func(ctx context.Context) error {
+		client, err := client.New(client.TokenFromEnv(false))
+		if err != nil {
+			return err
+		}
+
+		res, err := search.NewAPI(client).ByName(ctx, name)
+		if err != nil {
+			return err
+		}
+
+		vmAPI := vm.NewAPI(client)
+		for _, machine := range res {
+			// TODO wait for deprovision to be finished?
+			_, err := vmAPI.Deprovision(ctx, machine.Identifier, false)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
