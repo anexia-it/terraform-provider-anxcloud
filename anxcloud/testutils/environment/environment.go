c213741f0be1cdcd1a0183b1bbe2b68978a53464
kstiehl <kevin.stiehl@numericas.de>
2022-01-21 14:35:44 +0100

    add init

--- anxcloud/testutils/environment/environment.go
+++ anxcloud/testutils/environment/environment.go
@@ -2,12 +2,19 @@ package environment
 
 import (
 	"context"
+	"os"
+	"sync"
 )
 
 type Info struct {
 	Location string
 	VlanID   string
 }
+
+var (
+	envInfo *Info
+	mutex   sync.Mutex
+)
 
 type contextKey struct{}
 
@@ -17,4 +24,23 @@ func NewContext(ctx context.Context, environmentInfo Info) context.Context {
 
 func GetInfo(ctx context.Context) Info {
 	return *ctx.Value(contextKey{}).(*Info)
+}
+
+func init() {
+	if token := os.Getenv("ANEXIA_TOKEN"); token == "" {
+		return
+	}
+	// already initialised
+	if envInfo != nil {
+		return
+	}
+
+	// lock until end of function
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if envInfo != nil {
+		return
+	}
+	envInfo = &Info{}
 }
