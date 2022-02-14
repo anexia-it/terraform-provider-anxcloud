f26bd45d8364d8c3f020f97ae70dd231753a0e33
Kevin Stiehl <kevin.stiehl@numericas.de>
2022-02-14 11:24:36 +0100

    feat(tests): Add concept of Resource Recorders (#75)
    * add recorders
    
    * record servers
    
    * remove todo
    
    * remove unused code
    
    * add rebase
    
    * fix typo in function name
    
    * clean up go deps
    
    * fix vm_recorder
    
    * go mod tidy in tools
    
    * fix collecting errors

---
+++ anxcloud/testutils/recorder/vm_recorder.go
@@ -0,0 +1,77 @@
+package recorder
+
+import (
+	"context"
+	"errors"
+	"go.anx.io/go-anxcloud/pkg/client"
+	"go.anx.io/go-anxcloud/pkg/vsphere/provisioning/vm"
+	"go.anx.io/go-anxcloud/pkg/vsphere/search"
+	"net/http"
+)
+
+type vmCleanUpFunc func(ctx context.Context) error
+type VMRecoder struct {
+	handlers []vmCleanUpFunc
+}
+
+func (v VMRecoder) Cleanup(ctx context.Context) []error {
+	var cleanUpErrors []error
+	for _, handler := range v.handlers {
+		err := handler(ctx)
+		if err != nil {
+			cleanUpErrors = append(cleanUpErrors, err)
+		}
+	}
+	return cleanUpErrors
+}
+
+func (v *VMRecoder) RecordVMByName(name string) {
+	v.handlers = append(v.handlers, v.createCleanupHandlerByName(name))
+}
+
+func (v *VMRecoder) RecordVMByID(id string) {
+	v.handlers = append(v.handlers, v.createCleanupHandlerByID(id))
+}
+
+func (v VMRecoder) createCleanupHandlerByID(id string) vmCleanUpFunc {
+	return func(ctx context.Context) error {
+		anxClient, err := client.New(client.TokenFromEnv(false))
+		if err != nil {
+			return err
+		}
+
+		vmAPI := vm.NewAPI(anxClient)
+		_, err = vmAPI.Deprovision(ctx, id, false)
+
+		// it's not an error when it's gone
+		var responseErr client.ResponseError
+		if errors.As(err, &responseErr) && responseErr.Response.StatusCode != http.StatusNotFound {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func (v VMRecoder) createCleanupHandlerByName(name string) vmCleanUpFunc {
+	return func(ctx context.Context) error {
+		client, err := client.New(client.TokenFromEnv(false))
+		if err != nil {
+			return err
+		}
+
+		res, err := search.NewAPI(client).ByName(ctx, name)
+		if err != nil {
+			return err
+		}
+
+		vmAPI := vm.NewAPI(client)
+		for _, machine := range res {
+			_, err := vmAPI.Deprovision(ctx, machine.Identifier, false)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
