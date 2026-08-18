d08a338be4a1cc31e49dafc8626ee19e3d413322
Kim <kfehrs@anexia-it.com>
2026-08-18 15:16:13 +0200

    update the e2e test example to include more fields

--- tests/e2e/kubernetes/main.tf
+++ tests/e2e/kubernetes/main.tf
@@ -10,11 +10,25 @@ resource "anxcloud_kubernetes_cluster" "e2e" {
   needs_service_vms   = true
   enable_nat_gateways = true
   enable_lbaas        = var.enable_lbaas
-  enable_autoscaling  = false
+  enable_autoscaling  = true
 
-  cni_plugin                 = "canal"
-  external_ip_families       = var.external_ip_families
-  enable_oidc_authentication = false
+  cni_plugin           = "canal"
+  external_ip_families = var.external_ip_families
+
+  apiserver_allowlist = ["1.2.3.4"]
+
+  enable_oidc_authentication = true
+  oidc_client_id             = "1234"
+  oidc_issuer_url            = "https://localhost:8080"
+  oidc_groups_claim          = "groups"
+  oidc_username_claim        = "user"
+  oidc_extra_scopes          = "user,group"
+  oidc_groups_prefix         = "e2e_"
+  oidc_required_claim        = "group"
+  oidc_username_prefix       = "e2e_user"
+
+  maintenance_window_start_time = "Mon 00:00"
+  maintenance_window_duration   = "2h"
 
   wait_until_ready = var.wait_until_ready
 }
@@ -32,9 +46,9 @@ resource "anxcloud_kubernetes_node_pool" "e2e" {
 
   # These values are sent with the initial node-pool POST, including zeros.
   cpu_performance_type = "performance"
-  autoscaler_enabled   = false
-  autoscaler_min_nodes = 0
-  autoscaler_max_nodes = 0
+  autoscaler_enabled   = true
+  autoscaler_min_nodes = 1
+  autoscaler_max_nodes = 4
 
   # All node-pool networks are owned by the node pool and sent in its POST.
   networks {
