8602713d7a35faf6e2eab7c1899d74e10d2440b4
Kim Fehrs <kofl91@users.noreply.github.com>
2026-08-24 20:30:38 +0200

    docs: improve Kubernetes resource examples (#324)
    Signed-off-by: Kim Oliver Fehrs <kfehrs@Kims-MacBook-Air.local>
    Co-authored-by: Kim Oliver Fehrs <kfehrs@Kims-MacBook-Air.local>

--- examples/resources/anxcloud_kubernetes_cluster/resource.tf
+++ examples/resources/anxcloud_kubernetes_cluster/resource.tf
@@ -28,6 +28,7 @@ resource "anxcloud_kubernetes_cluster" "example" {
   external_ip_families       = "IPv4"
   enable_oidc_authentication = false
 
+  # Recommended: wait for cluster reconciliation to complete before Terraform returns.
   wait_until_ready = true
 }
 
